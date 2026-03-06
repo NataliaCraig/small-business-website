52dd0c8f4589641fcf83406567651750bae80478
Natalia <nataliacraig@usf.edu>
2026-03-06 11:31:14 -0500

    style css

--- cc_7.js
+++ cc_7.js
@@ -1,1 +1,12 @@
+// select elements
+const input = document.getElementById("headlineInput");
+const button = document.getElementById("updateBtn");
+const cta = document.getElementById("cta");
 
+// add event listener
+button.addEventListener("click", function() {
+    if (input.value.trim() !== "") {
+        cta.textContent = input.value;
+        input.value = "";
+    }
+});
